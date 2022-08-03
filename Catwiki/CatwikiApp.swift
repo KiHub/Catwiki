6935ab17d6f20ccd43afcaaba548d85e47c301a4
Kirill Ki <kirillkalapov@gmail.com>
2022-08-03 16:21:16 +0200

    Initial Commit

---
+++ Catwiki/CatwikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatwikiApp.swift
+//  Catwiki
+//
+//  Created by  Mr.Ki on 03.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct CatwikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
